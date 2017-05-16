a74bc5695f3b8e767084b9203041a0a22d8a6468
Montané Hamilton <montanehamilton@hotmail.com>
2017-05-15 19:31:11 -0600

    more skeleton

---
+++ VirtoCommerce.FedExModule.Web/VirtoCommerce.FedExModule.Web/Scripts/fedExModule.js
@@ -0,0 +1,15 @@
+﻿//Call this to register our module to main application
+var moduleName = "virtoCommerce.fedExModule";
+
+if (AppDependencies != undefined) {
+    AppDependencies.push(moduleName);
+}
+
+angular.module(moduleName, [
+])
+
+    .run(
+    ['platformWebApp.toolbarService', 'platformWebApp.dialogService', 'platformWebApp.mainMenuService', 'platformWebApp.widgetService', 'platformWebApp.authService', function (toolbarService, dialogService, avataxModuleResources, mainMenuService, widgetService, authService) {
+
+    }])
+    ;
--- VirtoCommerce.FedExModule.Web/VirtoCommerce.FedExModule.Web/Scripts/resources/fedExModuleApi.js
+++ VirtoCommerce.FedExModule.Web/VirtoCommerce.FedExModule.Web/Scripts/resources/fedExModuleApi.js
@@ -1,4 +1,4 @@
-﻿angular.module('platformWebApp.managedModule')
+﻿angular.module('platformWebApp.fedExModule')
 .factory('fedExModuleApi', ['$resource', function ($resource) {
   return $resource('api/fedExModule');
 }]);
